bfe756dfb2c012d6d5c26d1103e5dbae4a14dbaa
Shuayb A <shuayb@gmx.com>
2016-12-29 02:37:21 +0500

    ..

--- AlphaElectric/Forms/Home.xaml.cs
+++ AlphaElectric/Forms/Home.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AlphaElectric.Logic;
 
 namespace AlphaElectric.Forms
 {
@@ -22,6 +24,48 @@ namespace AlphaElectric.Forms
         public Home()
         {
             InitializeComponent();
+        }
+
+        private void GitHubButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/ButchersBoy/MaterialDesignInXamlToolkit");
+        }
+
+        private void TwitterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://twitter.com/James_Willock");
+        }
+
+        private void ChatButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://gitter.im/ButchersBoy/MaterialDesignInXamlToolkit");
+        }
+
+        private void EmailButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("mailto://james@dragablz.net");
+        }
+
+        private void DonateButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://pledgie.com/campaigns/31029");
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Hide();
+            var obj = new AddProduct();
+            obj.Show();
+            this.Close();
         }
     }
 }
---
+++ AlphaElectric/Logic/HomeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace AlphaElectric.Logic
+{
+
+}
