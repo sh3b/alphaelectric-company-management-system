36fa3e9aec3565e4a4cd1c747beadcf2ec6a37d8
Shuayb A <shuayb@gmx.com>
2016-12-29 21:40:38 +0500

    oh man.

--- AlphaElectric/Forms/2ndHome.xaml.cs
+++ AlphaElectric/Forms/2ndHome.xaml.cs
@@ -11,15 +11,14 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-// To access MetroWindow
-using MahApps.Metro.Controls;
 using System.ComponentModel;
-
-
-// Added DA & Factories
+using MahApps.Metro.Controls;
+using System.Diagnostics;
+using MaterialDesignThemes.Wpf;
+using System.Windows.Controls.Primitives;
 using AlphaElectric_DataAccessLayer;
 using AlphaElectric_DataAccessLayer.Factories;
-using System.Diagnostics;
+
 
 namespace AlphaElectric.Forms
 {
@@ -42,43 +41,78 @@ namespace AlphaElectric.Forms
             //worker.RunWorkerAsync();
         }
 
-        private void GitHubButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            Process.Start("https://github.com/ButchersBoy/MaterialDesignInXamlToolkit");
-        }
-
-        private void TwitterButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            Process.Start("https://twitter.com/James_Willock");
-        }
-
-        private void ChatButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            Process.Start("https://gitter.im/ButchersBoy/MaterialDesignInXamlToolkit");
-        }
-
-        private void EmailButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            Process.Start("mailto://james@dragablz.net");
-        }
-
-        private void DonateButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            Process.Start("https://pledgie.com/campaigns/31029");
-        }
-
-
-        private void TextBox_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            this.Hide();
-            var obj = new AddProduct();
-            obj.Show();
-            this.Close();
-        }
+        //private void TextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    this.Hide();
+        //    var obj = new AddProduct();
+        //    obj.Show();
+        //    this.Close();
+        //}
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
 
         }
+
+        private void ProductsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Employees_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChatButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PurchaseSellingButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProjectsButton_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #region diaglogandsocialbuttons
+
+        // About message dialog
+        private async void MenuPopupAboutButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var sMessageDialog = new MessageDialog
+            {
+                //Message = { Text = ((ButtonBase)sender).Content.ToString() }
+                Message = { Text = 
+                    "Developed by:\n" +
+                    "\n1. Muhammad Shoaib" +
+                    "\n  3022/FBAS/BSCS/F14B" +
+                    "\n2. Muhammad Amir" +
+                    "\n  ____/FBAS/BSCS/F14B" }
+            };
+
+            await DialogHost.Show(sMessageDialog, "RootDialog");
+        }
+
+        // Social Buttons
+        private void TwitterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("http://twitter.com/shuayb_ashraf");
+        }
+
+        private void ChatButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://gitter.im/shuayb_ashraf");
+        }
+
+        private void EmailButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("mailto://shuayb@gmx.com");
+        }
+        #endregion
     }
 }
