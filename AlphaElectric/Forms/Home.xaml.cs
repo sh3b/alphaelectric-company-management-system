d3d83b6c9a542c8233e21cf2cc03fe5fa970eb0c
Shuayb A <shuayb@gmx.com>
2016-12-29 22:30:48 +0500

    Home is ready.

--- AlphaElectric/Forms/Home.xaml.cs
+++ AlphaElectric/Forms/Home.xaml.cs
@@ -11,15 +11,14 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-// To access MetroWindow
-using MahApps.Metro.Controls;
 using System.ComponentModel;
-
-
-// Added DA & Factories
+using MahApps.Metro.Controls;
+using System.Diagnostics;
+using MaterialDesignThemes.Wpf;
+using System.Windows.Controls.Primitives;
 using AlphaElectric_DataAccessLayer;
 using AlphaElectric_DataAccessLayer.Factories;
-using System.Diagnostics;
+
 
 namespace AlphaElectric.Forms
 {
@@ -42,38 +41,106 @@ namespace AlphaElectric.Forms
             //worker.RunWorkerAsync();
         }
 
-        private void GitHubButton_OnClick(object sender, RoutedEventArgs e)
+        //private void TextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    this.Hide();
+        //    var obj = new AddProduct();
+        //    obj.Show();
+        //    this.Close();
+        //}
+
+        #region main-buttons
+
+        private void ButtonEmployees_Click(object sender, RoutedEventArgs e)
         {
-            Process.Start("https://github.com/ButchersBoy/MaterialDesignInXamlToolkit");
+
         }
 
+        private void ButtonProducts_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonCompanies_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonProjects_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPurchaseSelling_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region diaglog-socialbuttons-informationlinks
+
+        // About message dialog
+        private async void MenuPopupAboutButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var sMessageDialog = new MessageDialog
+            {
+                //Message = { Text = ((ButtonBase)sender).Content.ToString() }
+                Message = { Text = 
+                    "Developed by\n" +
+                    "\n1. Muhammad Shoaib" +
+                    "\n  3022/FBAS/BSCS/F14B" +
+                    "\n2. Muhammad Amir" +
+                    "\n  ____/FBAS/BSCS/F14B" }
+            };
+
+            await DialogHost.Show(sMessageDialog, "RootDialog");
+        }
+
+        // Social Buttons
         private void TwitterButton_OnClick(object sender, RoutedEventArgs e)
         {
-            Process.Start("https://twitter.com/James_Willock");
+            Process.Start("http://twitter.com/shuayb_ashraf");
         }
 
         private void ChatButton_OnClick(object sender, RoutedEventArgs e)
         {
-            Process.Start("https://gitter.im/ButchersBoy/MaterialDesignInXamlToolkit");
+            Process.Start("https://gitter.im/shuayb_ashraf");
         }
 
         private void EmailButton_OnClick(object sender, RoutedEventArgs e)
         {
-            Process.Start("mailto://james@dragablz.net");
+            Process.Start("mailto://shuayb@gmx.com");
         }
-
-        private void DonateButton_OnClick(object sender, RoutedEventArgs e)
+       
+        // Information Links
+        private void TextBlock_WebOrdersMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            Process.Start("https://pledgie.com/campaigns/31029");
+            Process.Start("http://alphaelectric.shuayb.me/");
         }
 
-
-        private void TextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        private async void TextBlock_FaxOrdersMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            this.Hide();
-            var obj = new AddProduct();
-            obj.Show();
-            this.Close();
+            var sMessageDialog = new MessageDialog
+            {
+                Message = { Text = "Fax Orders Server is currently\n offline." }
+            };
+
+            await DialogHost.Show(sMessageDialog, "RootDialog");
         }
+
+        private async void TextBlock_PhoneNumbersMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var sMessageDialog = new MessageDialog
+            {
+                Message = { Text =
+                    "Important Phone Numbers\n" +
+                    "\n1. Police: 15" +
+                    "\n2. IIUI:   +92 51 901 9100" }
+            };
+
+            await DialogHost.Show(sMessageDialog, "RootDialog");
+        }
+        #endregion
     }
 }
---
+++ AlphaElectric/Forms/oldHome.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+// To access MetroWindow
+using MahApps.Metro.Controls;
+using System.ComponentModel;
+
+
+// Added DA & Factories
+using AlphaElectric_DataAccessLayer;
+using AlphaElectric_DataAccessLayer.Factories;
+using System.Diagnostics;
+
+namespace AlphaElectric.Forms
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class oldHome : MetroWindow
+    {
+        public oldHome()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //Executing after loading window
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //worker = new BackgroundWorker();
+            //worker.DoWork += Worker_DoWork;
+            //worker.RunWorkerAsync();
+        }
+
+        private void GitHubButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://github.com/ButchersBoy/MaterialDesignInXamlToolkit");
+        }
+
+        private void TwitterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://twitter.com/James_Willock");
+        }
+
+        private void ChatButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://gitter.im/ButchersBoy/MaterialDesignInXamlToolkit");
+        }
+
+        private void EmailButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("mailto://james@dragablz.net");
+        }
+
+        private void DonateButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://pledgie.com/campaigns/31029");
+        }
+
+
+        private void TextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Hide();
+            var obj = new AddProduct();
+            obj.Show();
+            this.Close();
+        }
+    }
+}
